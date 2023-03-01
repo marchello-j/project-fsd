15b48c18f78346f0546c7bdf67c6f3549bba0502
Marsel <marchello.marsel@gmail.com>
2023-03-01 08:36:32 +0300

    Поравил импорт

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -1,21 +1,19 @@
 import { DeepPartial } from '@reduxjs/toolkit';
-import { LoginSchema } from '../types/loginSchema';
+import { loginSchema } from '../types/loginSchema';
 import { loginActions, loginReducer } from './loginSlice';
 
 describe('loginSlice.test', () => {
-    test('test set username', () => {
-        const state: DeepPartial<LoginSchema> = { username: '123' };
-        expect(loginReducer(
-            state as LoginSchema,
-            loginActions.setUsername('123123'),
-        )).toEqual({ username: '123123' });
-    });
+	test('test set username', () => {
+		const state: DeepPartial<loginSchema> = { username: '123' };
+		expect(loginReducer(state as loginSchema, loginActions.setUsername('123123'))).toEqual({
+			username: '123123',
+		});
+	});
 
-    test('test set password', () => {
-        const state: DeepPartial<LoginSchema> = { password: '123' };
-        expect(loginReducer(
-            state as LoginSchema,
-            loginActions.setPassword('123123'),
-        )).toEqual({ password: '123123' });
-    });
+	test('test set password', () => {
+		const state: DeepPartial<loginSchema> = { password: '123' };
+		expect(loginReducer(state as loginSchema, loginActions.setPassword('123123'))).toEqual({
+			password: '123123',
+		});
+	});
 });
